0c2623c55e0802307884aeea32ed50e15060539d
Usagizzz <94546936+Usagizzz@users.noreply.github.com>
2022-03-31 11:04:32 -0400

    ù;

---
+++ ProjetJava/src/launch/Executable.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package launch;
+
+import java.io.IOException;
+import viewmvc.Accueil;
+
+/**
+ *
+ * @author Clarence
+ */
+public class Executable {
+     
+   public static void main (String args []) throws IOException, InterruptedException{
+       Accueil accueil = new Accueil();
+       accueil.Accueil();  
+}  
+    
+}
