6c48ca9df2f8f8cbcd4cdc13e2c28776aa9055eb
Aym00n <remyj@outlook.fr>
2022-04-01 12:01:00 +0200

    maj promo

--- ProjetJava/src/launch/executable.java
+++ ProjetJava/src/launch/executable.java
@@ -15,6 +15,7 @@ import controlmvc.EmailVerif;
 import controlmvc.Fichier;
 import controlmvc.InsertImage;
 import controlmvc.ReadImage;
+import controlmvc.ReducPaiement;
 import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
@@ -31,17 +32,26 @@ public class Executable {
 
     public static void main(String args[]) throws IOException, InterruptedException, SQLException, SQLException, SQLException, SQLException, SQLException {
         Accueil accueil = new Accueil();
-        accueil.Accueil();
+       accueil.Accueil();
 
        ReservationDAOImpl r= new ReservationDAOImpl();
        ArrayList<Reservation> rl= new ArrayList<>();
-       
-       //rl=r.GetAllReservation();
-    
+       ClientsDAOImpl cdao= new ClientsDAOImpl();
+       Clients c= new Clients();
+//rl=r.GetAllReservation();
+    CityDAOImpl cidao= new CityDAOImpl();
+    City ci= new City();
        findUsersId id= new findUsersId();
        
        JLabel jlabel= new JLabel("VIP");
        
+       c=cdao.GetClient(1);
+       int promo=0,reduct=0;
+       ReducPaiement reduc= new ReducPaiement();
+       
+       //promo= reduc.ReducPaiement(c,cidao.GetCity(1));
+       
+       //System.out.println("promo: "+promo+" reduc: "+(int)((1-((float)promo/(float)(cidao.GetCity(1).GetPrix())))*100)+"%");
        //id.IdUser(jlabel);
     }
 }
